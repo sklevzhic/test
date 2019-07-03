fef72ddb166ea16d17441e6f7e891e7630e88fa7
Alexander Klevzhits <sklevzhic@gmail.com>
2019-07-03 15:48:53 +0300

    correction of errors clicks in the mobile version

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     $(".main-nav__item").mousemove(function(e) {
         !window.time || clearTimeout(time);
         time = setTimeout(function() {
@@ -15,14 +14,13 @@ $(document).ready(function() {
             .prev().removeClass('main-nav__link--active');
     });
 
-
     $('.main-nav__item--sub').on('touchstart', function(e) {
         e.preventDefault();
-        if ($('.main-nav__submenu').hasClass('show')) {
-            $(this).find(".main-nav__submenu")
-                .removeClass('show')
-                .prev().removeClass('main-nav__link--active');
-        } else {
+        let isClass = $(this).find(".main-nav__submenu").hasClass('show');
+        $(".main-nav__submenu.show")
+            .removeClass('show')
+            .prev().removeClass('main-nav__link--active');
+        if (!isClass) {
             $(this).find(".main-nav__submenu").addClass('show').prev().addClass('main-nav__link--active');
         }
     });
@@ -31,6 +29,14 @@ $(document).ready(function() {
         e.stopPropagation();
     });
 
+    $(document).on('touchstart', function(e) {
+        if (!$(e.target).is(".main-nav__link")) {
+            $(".main-nav__submenu.show")
+                .removeClass('show')
+                .prev().removeClass('main-nav__link--active');
+        }
+
+    });
 
     $(document).keyup(function(e) {
         if (e.which == 27) {
