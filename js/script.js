50903e6e69c6e20dd7ebf2cf642d0bc1e5e2f58f
Alexander Klevzhits <sklevzhic@gmail.com>
2019-07-03 14:52:50 +0300

    fix hide submenu

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
 
-    function hideSubMenu() {
-        $(this).find(".main-nav__submenu")
-            .removeClass('show')
-            .prev().removeClass('main-nav__link--active');
-    }
-
     $(".main-nav__item").mousemove(function(e) {
         !window.time || clearTimeout(time);
         time = setTimeout(function() {
@@ -15,12 +9,19 @@ $(document).ready(function() {
         }, 500);
     });
 
-    $(".main-nav__submenu, .main-nav__item").mouseleave(hideSubMenu);
+    $(".main-nav__submenu, .main-nav__item").mouseleave(function() {
+        $(this).find(".main-nav__submenu")
+            .removeClass('show')
+            .prev().removeClass('main-nav__link--active');
+    });
+
 
     $('.main-nav__item--sub').on('touchstart', function(e) {
         e.preventDefault();
         if ($('.main-nav__submenu').hasClass('show')) {
-            hideSubMenu();
+            $(this).find(".main-nav__submenu")
+                .removeClass('show')
+                .prev().removeClass('main-nav__link--active');
         } else {
             $(this).find(".main-nav__submenu").addClass('show').prev().addClass('main-nav__link--active');
         }
@@ -30,9 +31,12 @@ $(document).ready(function() {
         e.stopPropagation();
     });
 
+
     $(document).keyup(function(e) {
         if (e.which == 27) {
-            hideSubMenu();
+            $(this).find(".main-nav__submenu")
+                .removeClass('show')
+                .prev().removeClass('main-nav__link--active');
         }
     });
 
